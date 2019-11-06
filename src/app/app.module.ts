67cc0036fa9c3d301d62f2d5ccc10432405cbd73
stephanie Mach <stephaniemach86@yahoo.com>
2019-11-05 19:12:27 -0600

    Add SafePipe for YouTube embeds

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,9 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ApodComponent } from './apod/apod.component';
 
+//Import the pipe from node_modules
+import { SafePipeModule } from 'safe-pipe';
+
 // Relative import from the current directory to your PC's file system
 import { NgApodConfig } from '../../config/ng-apod.config';
 
@@ -19,7 +22,8 @@ import { NgApodConfig } from '../../config/ng-apod.config';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule //import HttpClientModule
+    HttpClientModule ,//import HttpClientModule
+    SafePipeModule //Add the pipe to your list of imports
   ],
   providers: [
     NgApodConfig //Add as a provider
